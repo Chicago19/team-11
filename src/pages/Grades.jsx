7e6b35cd69c50c9e7c1bbdabeab7306b41c986e2
Benjamin Cooper <bencooper222@gmail.com>
2019-09-20 18:49:09 -0500

    Finish basic react-router configuration

---
+++ src/pages/Grades.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Grades() {
+  return <div className="container">This is the grades.</div>;
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="container">
+      This is the homepage. It's probably the lowest priority.
+    </div>
+  );
+}
