daaa59cb34682ad792e254bd6f90143b433abf4d
Matthew Rosenbaum <mer5@illinois.edu>
2019-09-20 23:38:01 -0500

    added UI/UX for sending sms text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 export default function Home() {
   return (
     <div className="container">
-      This is the homepage. It's probably the lowest priority.
     </div>
   );
 }
