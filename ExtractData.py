768558ee9a9f2eccfb5f89f911a74874ab8b28ab
Paul J Moore <pjmoore92@gmail.com>
2019-10-18 17:12:55 +0100

    update

--- ExtractData.py
+++ ExtractData.py
@@ -1,5 +1,5 @@
 #
 from simple_salesforce import Salesforce
-sf = Salesforce(username='', password='', security_token='')
+sf = Salesforce(username='username', password='password', security_token='token')
 sf.Contact.create({'LastName':'Smith','Email':'example@example.com'})
 # this ended up as a dead end because the free version of salesforce, which we were using doesn't exteral apis
